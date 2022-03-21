f5a6ac174801e66fda9c66d280cc6081aaade789
nikhilg07 <43913706+nikhilg07@users.noreply.github.com>
2022-03-21 12:59:07 +0530

    Update SellerController.java

--- src/main/java/com/quinbay/Ecommerce/backend/controller/SellerController.java
+++ src/main/java/com/quinbay/Ecommerce/backend/controller/SellerController.java
@@ -1,4 +1,46 @@
 package com.quinbay.Ecommerce.backend.controller;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("seller")
 public class SellerController {
+    @Autowired
+    SellerService sellerService;
+
+    @Autowired
+    ProductService productService;
+
+
+    @PostMapping("/add-seller")
+    public void addSeller(@RequestBody Seller seller){
+        sellerService.addSeller(seller);
+    }
+
+    @PostMapping("/add-product")
+    public void addProduct(@RequestBody Product product){
+        productService.addProducts(product);
+    }
+
+    @GetMapping("/get-all-products")
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+    @GetMapping("/get-product-byId")
+    public Product getProductById(@RequestParam(value = "id") Long id){
+        return productService.getProductById(id);
+    }
+
+    @PutMapping("/update-product")
+    public void updateProduct(@RequestBody Product product){
+        productService.updateProduct(product);
+    }
+
+    @DeleteMapping("/delete-product")
+    public void deleteProduct(@RequestParam(value = "id") Long id)
+    {
+        productService.deleteProduct(id);
+    }
+
+
 }
